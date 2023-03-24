71ea452ab572ccc23074a86b08fab3fb2648fb3b
Asia Kaplanyan <a.kaplanyan@gmail.com>
2023-03-24 10:33:54 -0700

    add controllers

---
+++ Treats/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+using Microsoft.AspNetCore.Mvc;
+using Treats.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+using System.Security.Claims;
+
+namespace Treats.Controllers
+{
+    public class HomeController : Controller
+    {
+      private readonly TreatsContext _db;
+      private readonly UserManager<ApplicationUser> _userManager;
+
+      public HomeController(UserManager<ApplicationUser> userManager, TreatsContext db)
+      {
+        _userManager = userManager;
+        _db = db;
+      }
+      
+    [HttpGet("/")]
+    public async Task<ActionResult> Index()
+    {
+      Dictionary<string, object[]> model = new Dictionary<string, object[]>();
+      
+      //Treat logic
+      string userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+      ApplicationUser currentUser = await _userManager.FindByIdAsync(userId);
+      ViewBag.CurrentUser = userId;
+      if (currentUser != null)
+      {
+        Treat[] treats = _db.Treats
+                        .Where(entry => entry.User.Id == currentUser.Id)
+                        .ToArray();
+        model.Add("treats", treats);
+      }
+      return View(model);
+    }
+  }
+}
