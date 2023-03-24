08489e4119c282dddeaf1dde1a739f904964ee61
Asia Kaplanyan <a.kaplanyan@gmail.com>
2023-03-24 11:20:30 -0700

    BUGFIX: home page is not loading - Fixed on 03/24/2023

---
+++ Treats/Migrations/20230324181349_EditTreatModel.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Treats.Migrations
+{
+    public partial class EditTreatModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "TreatName",
+                table: "Treats",
+                newName: "Name");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "Treats",
+                newName: "TreatName");
+        }
+    }
+}
